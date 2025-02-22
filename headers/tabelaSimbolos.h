259f9e06ae02bc8fe458bef441fbb5ed0144969d
Caio Bonani <caio_bonani2002@hotmail.com>
2025-02-22 17:43:35 -0300

    upd semantico

--- headers/tabelaSimbolos.h
+++ headers/tabelaSimbolos.h
@@ -5,7 +5,6 @@
 
 extern int teveErroSemantico;
 
-/* Enumeração para os tipos de erro semântico */
 typedef enum {
     DeclVoidVar,
     DeclFuncExiste,
@@ -20,7 +19,6 @@ typedef enum {
     ChamadaFuncao
 } erroSemantico;
 
-/* Estrutura para armazenar informações do símbolo */
 struct SimboloTabela {
 
     char nomeID[MAXLEXEMA];
@@ -31,20 +29,14 @@ struct SimboloTabela {
 };
 typedef struct SimboloTabela SimboloTabela;
 
-/* Protótipos das funções de manipulação da tabela de símbolos */
 SimboloTabela** criarTabela(void);
-int proximoIndiceLivre(SimboloTabela** tabela);
 
 SimboloTabela** inserirSimbolo(SimboloTabela** tabela, const char* nomeID, const char* escopo, StatementTipo tipoID, Tipagem tipagem, int linha);
-int buscarSimbolo(SimboloTabela** tabela, const char* nomeID, const char* escopo); 
-int buscaIgual(SimboloTabela** tabela, const char* nomeID,const char* escopo,int linha);
-void printTabela(SimboloTabela** tabela);
-void liberarTabela(SimboloTabela** tabela);
 
-/* Protótipos das funções de análise semântica */
 void percorrerArvore(NoArvore* arvoreSintatica, SimboloTabela** tabela, char* escopo);
-void mostrarErroSemantico(erroSemantico erro, char* nome, int linha);
 
 int verificarMain(SimboloTabela** tabela);
+
+void printTabela(SimboloTabela** tabela);
 
 #endif
